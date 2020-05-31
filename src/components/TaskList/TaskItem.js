651c4b8fee4c5e0581e616e2f2f46255d33978e9
Denise Rosegg <denise@reallygood.co.il>
2020-05-31 22:53:15 +0300

    Adds `onClick` functionality to `TaskItem`

--- src/components/TaskList/TaskItem.js
+++ src/components/TaskList/TaskItem.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import cc from 'classcat';
+import walkme from '@walkme/sdk';
 
 import classes from './styles.module.scss';
 
 export default function TaskItem({ node }) {
+  function onClick() {
+    walkme.content.playById(walkme.content.TypeNames.Task, node.id);
+    walkme.platform.closeMe();
+  }
+
   return (
     <li
       className={cc([
@@ -14,6 +20,7 @@ export default function TaskItem({ node }) {
           [classes['is-disabled']]: node.properties.isDisabled,
         },
       ])}
+      onClick={onClick}
     >
       <span className={classes.title}>{node.title}</span>
     </li>
