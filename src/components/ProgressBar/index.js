cb7de27e23bb62a3fff8947689c740e1123d93e7
Denise Rosegg <denise@reallygood.co.il>
2020-06-01 09:14:22 +0300

    Adds `ProgressBar` component and uses it in `TaskList`

---
+++ src/components/ProgressBar/index.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+
+import classes from './styles.module.scss';
+
+export default function ProgressBar({ percent }) {
+  const [completedPercent, setCompletedPercent] = useState(0);
+
+  useEffect(() => {
+    setCompletedPercent(percent);
+  }, []);
+
+  return (
+    <div className={classes['progress-bar']}>
+      <div className={classes.title}>{completedPercent}% completed</div>
+      <div className={classes['progress-bar-indicator']} style={{ '--indicator-percent': `${completedPercent}%` }} />
+    </div>
+  );
+}
